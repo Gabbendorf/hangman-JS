3bf6bca6285213d9c35ad9d13ea17c1f24540e8c
Gabba <gmedas@8thlight.com>
2018-03-20 16:10:20 +0000

    Minor changes

--- test/guessesFormatter.spec.js
+++ test/guessesFormatter.spec.js
@@ -1,8 +1,7 @@
 import {GuessesFormatter} from '../src/guessesFormatter'
 
-test("formats the wrong guesses", () => {
-  let wrongGuesses = ["a", "b"]
-  let guessesFormatter = new GuessesFormatter
+test("formats the list of guesses", () => {
+  const guessesFormatter = new GuessesFormatter
 
-  expect(guessesFormatter.formatted(wrongGuesses)).toBe("A B")
+  expect(guessesFormatter.formatted(["a", "b"])).toBe("A B")
 })
